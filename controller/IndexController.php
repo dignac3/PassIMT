c47ad6dd0f47286e5522d6ed75ef96c46d343a31
dodo <dodo>
2020-02-05 11:55:27 +0100

    add Router

---
+++ controller/IndexController.php
@@ -0,0 +1,18 @@
+<?php
+
+class IndexController
+{
+
+
+    /**
+     * IndexController constructor.
+     */
+    public function __construct()
+    {
+    }
+
+    public function getIndex()
+    {
+
+    }
+}
--- index.php
+++ index.php
@@ -1,13 +1,20 @@
-<!doctype html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Password Manager</title>
-</head>
-<body>
-    <h1>Pass IMT</h1>
-</body>
-</html>
+<?php
+
+include_once 'router/Request.php';
+include_once 'router/Router.php';
+$router = new Router(new Request);
+
+$router->get('/', function() {
+  return <<<HTML
+  <h1>Hello world</h1>
+HTML;
+});
+
+
+$router->get('/profile', function($request) {
+  return <<<HTML
+  <h1>Profile</h1>
+HTML;
+});
+
+?>
