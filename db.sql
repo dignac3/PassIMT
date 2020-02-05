0708776cb66e25d76b427a64888a70f08d341005
Guitou <guillaume.lagay@etu.univ-amu.fr>
2020-02-05 11:03:47 +0100

    feat: add database structure tables

---
+++ db.sql
@@ -0,0 +1,16 @@
+CREATE TABLE users (
+  id int NOT NULL AUTO_INCREMENT,
+  mail TEXT NOT NULL,
+  master_password TEXT NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE passwords (
+  id int NOT NULL AUTO_INCREMENT,
+  user_id int,
+  label TEXT NOT NULL,
+  login TEXT,
+  password TEXT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_id) REFERENCES users(id)
+);
